a6c857ae0eca13c11b6b27d5a409e81488b337fa
achmadrizky27 <rizkyach27@gmail.com>
2024-10-16 21:11:11 +0700

    ..

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -58,8 +58,8 @@ const NavbarComponent = () => {
   }, []);
 
   return (
-    <nav className="fixed top-0 w-full font-poppins tracking-wider z-50">
-      <div className={`max-w-7xl mx-auto px-3 md:px-7 ${navBackground}`}>
+    <nav className={`fixed top-0 w-full font-poppins tracking-wider z-50 ${navBackground}`}>
+      <div className="max-w-7xl mx-auto px-3 md:px-7">
         <div className="flex justify-between items-center py-5">
           <div className="flex gap-2 items-center">
             <img src="../profile/logo-non-bg.png" alt="logo-non-bg" width={30} />
