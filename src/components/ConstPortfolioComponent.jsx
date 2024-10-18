2ad454961ed9b2dbbae009477b7236513b61f178
achmadrizky27 <rizkyach27@gmail.com>
2024-10-18 11:28:00 +0700

    First Commit

--- src/components/ConstPortfolioComponent.jsx
+++ src/components/ConstPortfolioComponent.jsx
@@ -1,24 +1,55 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const ConstPortfolioComponent = ({ target, duration }) => {
   const [count, setCount] = useState(0);
+  const [inView, setInView] = useState(false);
+  const elementRef = useRef(null);
 
   useEffect(() => {
-    let start = 0;
-    const increment = target / (duration / 10);
-    const timer = setInterval(() => {
-      start += increment;
-      if (start >= target) {
-        start = target;
-        clearInterval(timer);
+    // Intersection Observer untuk memantau elemen
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0];
+        setInView(entry.isIntersecting);
+      },
+      { threshold: 0.5 } // Elemen harus terlihat setidaknya 50% untuk dianggap masuk viewport
+    );
+
+    if (elementRef.current) {
+      observer.observe(elementRef.current);
+    }
+
+    return () => {
+      if (elementRef.current) {
+        observer.unobserve(elementRef.current);
       }
-      setCount(Math.ceil(start));
-    }, 10);
+    };
+  }, []);
 
-    return () => clearInterval(timer);
-  }, [target, duration]);
+  useEffect(() => {
+    if (!inView) {
+      setCount(0); // Reset count ketika elemen keluar dari viewport
+    } else {
+      let start = 0;
+      const increment = target / (duration / 10);
+      const timer = setInterval(() => {
+        start += increment;
+        if (start >= target) {
+          start = target;
+          clearInterval(timer);
+        }
+        setCount(Math.ceil(start));
+      }, 10);
 
-  return <span className="text-3xl">{count}</span>;
+      return () => clearInterval(timer);
+    }
+  }, [inView, target, duration]);
+
+  return (
+    <span className="text-3xl" ref={elementRef}>
+      {count}
+    </span>
+  );
 };
 
 export default ConstPortfolioComponent;
